eb81ebb1e989958601bbeedbd0b56a1803be1977
Óscar Barahona <obarahona10@outlook.com>
2026-03-03 12:49:18 +0100

    fix: validate current step before on submit
    Inspired by https://github.com/jazzband/django-formtools/pull/224
    Thank you @jsma

--- formtools/wizard/forms.py
+++ formtools/wizard/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class ManagementForm(forms.Form):
     """
     ``ManagementForm`` is used to keep track of the current wizard step.
     """
+
     template_name = "django/forms/p.html"  # Remove when Django 5.0 is minimal version.
     current_step = forms.CharField(widget=forms.HiddenInput)
+
+    def __init__(self, steps, **kwargs):
+        self.steps = steps
+        super().__init__(**kwargs)
+
+    def clean_current_step(self):
+        value = self.cleaned_data["current_step"]
+        if value not in self.steps:
+            raise ValidationError("Invalid step name.")
+        return value
